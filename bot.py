2d2d3306fd05d38a9cb1ab9c9238c4b53aea3f28
fanofsuperman <larkfanofsuperman@gmail.com>
2026-01-09 00:00:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
 from telethon import TelegramClient
-import asyncio
+from telethon.sessions import StringSession
+import asyncio, os, random
 
 api_id = 24751547
 api_hash = "a125209289c5281b43a67d02019aa97f"
 
-chat_id = -1003621946413  # CHANGE THIS to your target channel/group ID
+chat_id = -1003621946413
+session = os.getenv("SESSION")
 
-client = TelegramClient("grow_session", api_id, api_hash)
+client = TelegramClient(StringSession(session), api_id, api_hash)
 
 async def main():
     await client.start()
-    print("Logged in. Auto /grow started.")
+    print("24/7 auto /grow started")
 
     while True:
         try:
             await client.send_message(chat_id, "/grow")
-            print("Sent /grow")
-            await asyncio.sleep(30)
+            delay = random.randint(180, 240)
+            await asyncio.sleep(delay)
         except Exception as e:
-            print("Error:", e)
-            await asyncio.sleep(60)
+            print(e)
+            await asyncio.sleep(120)
 
 client.loop.run_until_complete(main())
